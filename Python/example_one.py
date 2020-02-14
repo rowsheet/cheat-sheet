d51cadb833d3bb2db7c73f9c9517c15abe3cac21
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2020-02-14 14:16:12 -0700

    Adding Python 1.

---
+++ Python/example_one.py
@@ -0,0 +1,9 @@
+
+
+"""
+Don't worrie too much about what this means, this is just the "python" way
+of setting the entry point of the file. When you run this python file
+as is (versus including it from another file), it will run this protion.
+"""
+if __name__ == "__main__":
+    print("TESTING")
