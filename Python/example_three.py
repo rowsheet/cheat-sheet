ed5b362ee8b0481c2d7fe0cb9336e81320f2ea83
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2020-02-14 14:26:27 -0700

    Adding python 3

---
+++ Python/example_three.py
@@ -0,0 +1,36 @@
+def example_function():
+    print("THIS IS AN EXAMPLE FUNCTION")
+
+def read_antibiotics_file_and_print():
+    """
+    Open the file. Note: this assumes the filename and that it exits. If the
+    file doesn't exist, this will cause an error.
+    """
+    with open("antibiotics.csv") as antibiotics_file:
+        """
+        Read the file contents.
+        """
+        file_contents = antibiotics_file.read()
+        """
+        Collect an array of the lines by taking all of the file contents and
+        splitting by a new-line charachter (aka "carage return")
+        """
+        lines = file_contents.split("\n")
+        """
+        Go through each line.
+        """
+        for line in lines:
+            """
+            Print the line to the screen.
+            """
+            print(line)
+
+"""
+Don't worrie too much about what this means, this is just the "python" way
+of setting the entry point of the file. When you run this python file
+as is (versus including it from another file), it will run this protion.
+"""
+if __name__ == "__main__":
+    print("TESTING")
+    example_function()
+    read_antibiotics_file_and_print()
